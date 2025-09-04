6b5b4c3f5cb7d6a6df16670f1d9db5b9981c53df
Omdas2604 <dasswayenshuom@gmail.com>
2025-09-04 22:32:48 +0530

    fixing cors issues

--- src/app.js
+++ src/app.js
@@ -7,12 +7,23 @@ const postRoutes = require('./routes/post.routes');
 
 const app = express();
 
-// 2. Configure and use the cors middleware
-app.use(cors({
-    origin: 'http://localhost:5173', // The address of your frontend app
-    credentials: true
-}));
+const allowedOrigins = [
+  'http://localhost:5173',      // Your local Vite dev server
+  'https://omdas2604.github.io'  // Your deployed GitHub Pages site
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the incoming origin is on the guest list or if it's not a browser request (like from Postman)
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('This origin is not allowed by CORS'));
+    }
+  },
+  credentials: true // This is essential for allowing cookies and authorization headers
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
